16467075985fef5406292c276524f44059b43157
FrenchHelp <Ryanc6145@gmail.com>
2025-05-18 14:33:25 -0400

    Update socket.js

--- public/socket.js
+++ public/socket.js
@@ -1,14 +1,23 @@
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) {
-    if (users[username].password === password) {
-      return res.json({ success: true });
-    } else {
-      return res.status(401).json({ success: false, message: 'Incorrect password.' });
-    }
-  } else {
-    users[username] = { password, chats: [] };
-    fs.writeFileSync('./users.json', JSON.stringify(users));
-    return res.json({ success: true });
-  }
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+  });
+
+  socket.on('chatMessage', ({ room, user, message }) => {
+    io.to(room).emit('chatMessage', { user, message });
+  });
+
+  socket.on('videoControl', ({ room, action, time }) => {
+    io.to(room).emit('videoControl', { action, time });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`VibeRoom server running on port ${PORT}`);
 });
