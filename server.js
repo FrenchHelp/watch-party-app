d1c4b470839337783cf8ebf7167b9895df7a9fe8
FrenchHelp <Ryanc6145@gmail.com>
2025-05-18 15:11:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,69 +8,132 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-// Serve static files from public folder
+// Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
+
+const rooms = {};
+
+// Helper to ensure room data exists
+function createRoomIfNotExists(room) {
+  if (!rooms[room]) {
+    rooms[room] = {
+      users: {},
+      videoID: null,
+      videoState: 'paused', // 'playing' or 'paused'
+      videoTime: 0,
+    };
+  }
+}
 
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
 
-  // Join a room
+  // User joins a room
   socket.on('joinRoom', ({ username, room }) => {
+    createRoomIfNotExists(room);
     socket.join(room);
-    socket.username = username;
-    socket.room = room;
 
-    console.log(`${username} joined room: ${room}`);
+    // Store user info
+    rooms[room].users[socket.id] = username;
 
-    // Notify others in room
-    socket.to(room).emit('chatMessage', {
-      username: 'System',
+    // Send current video state to new user
+    const { videoID, videoState, videoTime } = rooms[room];
+    socket.emit('loadVideo', videoID);
+    if (videoID) {
+      if (videoState === 'playing') {
+        socket.emit('videoPlay', videoTime);
+      } else {
+        socket.emit('videoPause', videoTime);
+      }
+    }
+
+    // Notify room about new user
+    io.to(room).emit('chatMessage', {
+      username: 'VibeRoom Bot',
       message: `${username} has joined the room.`,
     });
 
-    // Optionally send back current room info or users list
+    console.log(`${username} joined room ${room}`);
   });
 
-  // Chat message received
+  // Chat messages
   socket.on('chatMessage', (msg) => {
-    const room = socket.room;
-    if (room) {
-      io.to(room).emit('chatMessage', {
-        username: socket.username,
-        message: msg,
-      });
-    }
+    const userRoom = Array.from(socket.rooms).find((r) => r !== socket.id);
+    if (!userRoom) return;
+
+    const username = rooms[userRoom]?.users[socket.id] || 'Anonymous';
+
+    io.to(userRoom).emit('chatMessage', { username, message: msg });
   });
 
-  // Video controls: play
-  socket.on('videoPlay', (currentTime) => {
-    socket.to(socket.room).emit('videoPlay', currentTime);
+  // Video events
+  socket.on('loadVideo', (videoID) => {
+    const userRoom = Array.from(socket.rooms).find((r) => r !== socket.id);
+    if (!userRoom) return;
+
+    createRoomIfNotExists(userRoom);
+    rooms[userRoom].videoID = videoID;
+    rooms[userRoom].videoState = 'paused';
+    rooms[userRoom].videoTime = 0;
+
+    // Broadcast to all others in room
+    socket.to(userRoom).emit('loadVideo', videoID);
   });
 
-  // Video controls: pause
-  socket.on('videoPause', (currentTime) => {
-    socket.to(socket.room).emit('videoPause', currentTime);
+  socket.on('videoPlay', (time) => {
+    const userRoom = Array.from(socket.rooms).find((r) => r !== socket.id);
+    if (!userRoom) return;
+
+    rooms[userRoom].videoState = 'playing';
+    rooms[userRoom].videoTime = time;
+
+    socket.to(userRoom).emit('videoPlay', time);
   });
 
-  // Video controls: seek
+  socket.on('videoPause', (time) => {
+    const userRoom = Array.from(socket.rooms).find((r) => r !== socket.id);
+    if (!userRoom) return;
+
+    rooms[userRoom].videoState = 'paused';
+    rooms[userRoom].videoTime = time;
+
+    socket.to(userRoom).emit('videoPause', time);
+  });
+
   socket.on('videoSeek', (time) => {
-    socket.to(socket.room).emit('videoSeek', time);
+    const userRoom = Array.from(socket.rooms).find((r) => r !== socket.id);
+    if (!userRoom) return;
+
+    rooms[userRoom].videoTime = time;
+    socket.to(userRoom).emit('videoSeek', time);
   });
 
-  // Disconnect
+  // User disconnects
   socket.on('disconnect', () => {
-    if (socket.room) {
-      socket.to(socket.room).emit('chatMessage', {
-        username: 'System',
-        message: `${socket.username} has left the room.`,
-      });
+    for (const room in rooms) {
+      if (rooms[room].users[socket.id]) {
+        const username = rooms[room].users[socket.id];
+        delete rooms[room].users[socket.id];
+
+        io.to(room).emit('chatMessage', {
+          username: 'VibeRoom Bot',
+          message: `${username} has left the room.`,
+        });
+
+        console.log(`${username} disconnected from room ${room}`);
+
+        // If room is empty, delete it
+        if (Object.keys(rooms[room].users).length === 0) {
+          delete rooms[room];
+          console.log(`Deleted empty room: ${room}`);
+        }
+        break;
+      }
     }
-    console.log('Client disconnected:', socket.id);
   });
 });
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`VibeRoom server running on port ${PORT}`);
 });
