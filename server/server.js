ac5303dbcfb3d44cca76977adf202d19c0bea478
FrenchHelp <Ryanc6145@gmail.com>
2025-05-18 14:58:33 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,32 +1,79 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-const fs = require('fs');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.json());
 
-let users = {};
-if (fs.existsSync('./users.json')) {
-  users = JSON.parse(fs.readFileSync('./users.json'));
-}
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) {
-    if (users[username].password === password) {
-      return res.json({ success: true });
-    } else {
-      return res.status(401).json({ success: false, message: 'Incorrect password.' });
+const rooms = {}; // { roomId: { videoState, users: Set } }
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId, username) => {
+    socket.join(roomId);
+    socket.username = username;
+    socket.roomId = roomId;
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = {
+        videoState: {
+          playing: false,
+          currentTime: 0,
+          videoId: 'dQw4w9WgXcQ' // default video (Rick Astley!)
+        },
+        users: new Set(),
+      };
     }
-  } else {
-    users[username] = { password, chats: [] };
-    fs.writeFileSync('./users.json', JSON.stringify(users));
-    return res.json({ success: true });
-  }
+
+    rooms[roomId].users.add(socket.id);
+
+    // Send current video state to this user
+    socket.emit('video-state', rooms[roomId].videoState);
+
+    // Notify room about new user
+    io.to(roomId).emit('chat-message', {
+      username: 'System',
+      message: `${username} joined the room`,
+    });
+
+    // Update user list
+    io.to(roomId).emit('user-list', Array.from(rooms[roomId].users));
+  });
+
+  socket.on('chat-message', msg => {
+    if (socket.roomId) {
+      io.to(socket.roomId).emit('chat-message', {
+        username: socket.username,
+        message: msg,
+      });
+    }
+  });
+
+  socket.on('video-control', data => {
+    // Update room video state
+    if (socket.roomId && rooms[socket.roomId]) {
+      rooms[socket.roomId].videoState = { ...rooms[socket.roomId].videoState, ...data };
+      // Broadcast to everyone else in the room
+      socket.to(socket.roomId).emit('video-state', rooms[socket.roomId].videoState);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    if (socket.roomId && rooms[socket.roomId]) {
+      rooms[socket.roomId].users.delete(socket.id);
+      io.to(socket.roomId).emit('chat-message', {
+        username: 'System',
+        message: `${socket.username} left the room`,
+      });
+      io.to(socket.roomId).emit('user-list', Array.from(rooms[socket.roomId].users));
+    }
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
