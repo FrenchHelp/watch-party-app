b0700f5e8f8cb8855f1d42f60cd28918d3858d1c
FrenchHelp <Ryanc6145@gmail.com>
2025-05-18 14:52:02 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,3 +16,17 @@ let users = {};
 if (fs.existsSync('./users.json')) {
   users = JSON.parse(fs.readFileSync('./users.json'));
 }
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  if (users[username]) {
+    if (users[username].password === password) {
+      return res.json({ success: true });
+    } else {
+      return res.status(401).json({ success: false, message: 'Incorrect password.' });
+    }
+  } else {
+    users[username] = { password, chats: [] };
+    fs.writeFileSync('./users.json', JSON.stringify(users));
+    return res.json({ success: true });
+  }
+});
